fe03c8a8f4f4de0fb070addf31bb5fd175f0a529
Darthlex29 <david.dam37@gmail.com>
2026-08-14 08:20:00 -0500

    chore: inicializacion del proyecto

---
+++ Backend/customersKata/src/test/java/com/bancobogota/customersKata/CustomersKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancobogota.customersKata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
